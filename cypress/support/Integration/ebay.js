27e20aa63768b4e89f03dd2bdd0a961c67a176e0
Vikash Kumar <165239080+iRVikashSingh@users.noreply.github.com>
2024-09-28 19:00:28 +0530

    Update ebay.js

--- cypress/support/Integration/ebay.js
+++ cypress/support/Integration/ebay.js
@@ -6,10 +6,13 @@ describe('ebay search test', () => {
         cy.get("#gh-as-a").click();
 
         //select catagory
-        cy.get("[id='s0-1-17-4[0]-7[3]-_sacat']").should('have.value','2984');
+        //cy.get("[id='s0-1-17-4[0]-7[3]-_sacat']").should('have.value','2984');
+
+        cy.get("[id='s0-1-17-4[0]-7[3]-_sacat']").select("Baby");
 
         //click on search button
-        cy.get("[class='btn btn--primary']").select('0').click();
+        //cy.get("[class='btn btn--primary']").select('0').click();
+        cy.get('input[id="s0-1-17-6[4]-[0]-LH_ItemCondition"]').click()
         
     });
 });
